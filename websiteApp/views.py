5cdf3a0915c49cf610e81ddfd04783839ac99b1c
Christian Aarón  Palacios Velazco <capv5001@gmail.com>
2022-01-08 14:45:53 -0500

    traspaso de templates/websiteApp/servicios.html a templates/servicios/servicios.html

--- websiteApp/views.py
+++ websiteApp/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render,HttpResponse
 from websiteApp import urls
-from servicios.models import Servicio
 
 
 # Create your views here.
 
 def home(request):
     return render(request,'websiteApp/home.html')
-
-def servicios(request):
-    servicios=Servicio.objects.all()
-    return render(request,'websiteApp/servicios.html',{
-        'servicios':servicios
-    })
 
 def tienda(request):
     return render(request,'websiteApp/tienda.html')
