2c456b296a4faada37c6aa8b63c3db505f5e3edf
Christian Aarón  Palacios Velazco <capv5001@gmail.com>
2022-01-08 22:35:38 -0500

    create a bucle to show entries in blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from blog.models import Categoria,Post
 
 # Create your views here.Z
 def blog(request):
-    return render(request,'blog/blog.html')
+    categorias=Categoria.objects.all()
+    posts=Post.objects.all()
+    return render(request,'blog/blog.html',{
+        'categorias':categorias,
+        'posts':posts,
+    })
